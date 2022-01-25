b5989b610a22cca452a129c786655d1c5e9aa46e
Cormen19 <76107484+Cormen19@users.noreply.github.com>
2022-01-25 12:45:35 +0100

    cambios en el nav y masterpage

--- Laravel/resources/views/layout/masterpage.blade.php
+++ Laravel/resources/views/layout/masterpage.blade.php
@@ -5,6 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Gure Tabadul-@yield('Titulo')</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Paprika&family=Poppins:wght@300&display=swap" rel="stylesheet">
+    <!-- Librerias -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+    <!-- Links CSS | JS -->
+    <link rel="stylesheet" href="css/EstilosModificado.css">
 </head>
 <body background=img/fondo_GureTabadul.jpg style="background-size:100%; background-image: cover;background-repeat: no-repeat">
     @include('layout.nav')
