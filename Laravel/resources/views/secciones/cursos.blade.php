36aafc76d8cb85a9d601e8a88340b839743dc40a
jordioni1180 <jordioni1705@gmail.com>
2022-02-02 13:23:25 +0100

    Update cursos.blade.php

--- Laravel/resources/views/secciones/cursos.blade.php
+++ Laravel/resources/views/secciones/cursos.blade.php
@@ -1,15 +1,10 @@
 @extends('layout.masterpage')
 @section('Titulo','Cursos')
 @section('contenido')
-<h2>Esta es la pagina de cursos</h2>
-<select name="elegir">
-<option value="Todo" selected></option>
-  <option value="Cursos"></option>
-  <option value="Talleres"></option>
-</select>
+
 @foreach ($cursos as $curso)
-<div class="cursos"><a href="{{route('cursos.show')}}">
-<img src={{$curso->imagenes->first()->ruta}}>
+<div class="cursos"><a href="./cursos/{{$taller->id}}">
+{{-- <img src={{$curso->imagenes->first()->ruta}}> --}}
 <h1>{{$curso->nombre}}</h1>
 </a></div>
 @endforeach
