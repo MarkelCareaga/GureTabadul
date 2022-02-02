8834524912363457f17c911961c7643a97cb859a
Cormen19 <76107484+Cormen19@users.noreply.github.com>
2022-02-02 09:14:35 +0100

    nav y borrao registroController

--- Laravel/resources/views/layout/nav.blade.php
+++ Laravel/resources/views/layout/nav.blade.php
@@ -10,21 +10,15 @@
         <li class="nav-item active">
           <a class="nav-link" aria-current="page" href="{{route('inicio.index')}}">Inicio</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="{{route('servicios.index')}}">Servicios</a>
-        </li>
+        
         <li class="nav-item">
           <a class="nav-link" aria-current="page" href="{{route('cursos.index')}}">Cursos</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" aria-current="page" href="#">Tienda</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="{{route('nosotros.index')}}">Nosotros</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="{{route('sponsors.index')}}">Sponsors</a>
-        </li>
+        
+        
         <li class="nav-item">
           <a class="nav-link" aria-current="page" href="{{route('login.index')}}">Login</a>
         </li>
