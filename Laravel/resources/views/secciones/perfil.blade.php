d46b69f7e87d692b696d44e33088f031716caad7
Cormen19 <76107484+Cormen19@users.noreply.github.com>
2022-02-07 13:50:52 +0100

    scss

--- Laravel/resources/views/secciones/perfil.blade.php
+++ Laravel/resources/views/secciones/perfil.blade.php
@@ -1,67 +1,9 @@
 @extends ('layout.masterpage')
 @section ('contenido')
+@section('estilos')
+<link rel="stylesheet" href="{{URL::asset('css/perfil.css') }}">
+@endsection
 
-<style>
-  body{
-    margin-top:20px;
-    color: #1a202c;
-    text-align: left;
-    background-color: #e2e8f0;
-}
-.main-body {
-    padding: 15px;
-}
-.card {
-    box-shadow: 0 1px 3px 0 rgba(0,0,0,.1), 0 1px 2px 0 rgba(0,0,0,.06);
-}
-
-.card {
-    position: relative;
-    display: flex;
-    flex-direction: column;
-    min-width: 0;
-    word-wrap: break-word;
-    background-color: #3AA877;
-    background-clip: border-box;
-    border: 0 solid rgba(0,0,0,.125);
-    border-radius: .25rem;
-}
-
-.card-body {
-    flex: 1 1 auto;
-    min-height: 1px;
-    padding: 1rem;
-}
-
-.gutters-sm {
-    margin-right: -8px;
-    margin-left: -8px;
-}
-
-.gutters-sm>.col, .gutters-sm>[class*=col-] {
-    padding-right: 8px;
-    padding-left: 8px;
-}
-.mb-3, .my-3 {
-    margin-bottom: 1rem!important;
-}
-
-.bg-gray-300 {
-    background-color: #e2e8f0;
-}
-.h-100 {
-    height: 100%!important;
-}
-.shadow-none {
-    box-shadow: none!important;
-}
-div.card-body{
-  height: 509px;
-  padding-top:89px;
-}
-
-
-</style>
 
 
 <div class="container">
@@ -76,12 +18,7 @@ div.card-body{
             <div class="card">
               
               <div class="card-body">
-                <div class="d-flex flex-column align-items-center text-center">
-                  <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Admin" class="rounded-circle" width="150">
-                  <div class="mt-3">
-                    <h4>{{$usuario->name}}</h4>
-                  </div>
-                </div>
+                <input type="button" name="botonEditarPerfil" value="Editar perfil" id="botonEditar1" onclick="botonEditarPerfil()">
               </div>
             </div>
 
