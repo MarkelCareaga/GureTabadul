43ac830e71a3a1e4aa69db1d033d92633c8a4ecd
jordioni1180 <jordioni1705@gmail.com>
2022-02-08 13:47:12 +0100

    curso css

--- Laravel/resources/views/secciones/cursos.blade.php
+++ Laravel/resources/views/secciones/cursos.blade.php
@@ -9,7 +9,7 @@
 
 <div class="card" style="width: 18rem;">
   <a href="{{route('cursos.show',['curso'=>$curso])}}">
- 
+
     <img class="imagen" src="storage/{{$curso->imagenes()->first()->ruta}}" class="card-img-top" alt="">
 
     </a>
@@ -20,10 +20,10 @@
       <p class="card-text"><i class="fas fa-info-circle"></i> {{$curso->descripcion}}</p>
       <p class="card-text"><i class="fas fa-coins"></i>&nbsp;&nbsp;{{$curso->precio}}&nbsp;&nbsp;<i class="fas fa-euro-sign"></i></p>
       <p class="card-text"><i class="fas fa-female"></i>&nbsp;&nbsp;&nbsp;&nbsp;{{$curso->capacidad}}</p>
-      
+
     </div>
   </div>
-  
+
 @endforeach
 </div>
 
