25f96852f87861da9a983e25aaf5e0328771abbb
Cormen19 <76107484+Cormen19@users.noreply.github.com>
2022-02-09 00:26:48 +0100

    footer retocado sin el mapa y perfil responsive

--- Laravel/resources/views/layout/footer.blade.php
+++ Laravel/resources/views/layout/footer.blade.php
@@ -1,9 +1,9 @@
 <link rel="stylesheet" href="{{URL::asset('css/footer.css')}}">
-    <footer style="height: 200px">
+    {{-- <footer style="height: 200px">
         <div class="content">
         <div class="logo"><img src="{{URL::asset('img/LogoTabadul64PX.jpg')}}"></div>
         <div class="mapa">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2905.516500040551!2d-2.9416024847722677!3d43.26155408568574!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd4e502873b41fd5%3A0x1ab0ace3022b6ab9!2sCentro%20San%20Luis!5e0!3m2!1ses!2ses!4v1644345605575!5m2!1ses!2ses" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy"></iframe>        </div>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d305.4047332794024!2d-2.9395107089456576!3d43.26156761932582!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd4e502873b41fd5%3A0x1ab0ace3022b6ab9!2sCentro%20San%20Luis!5e0!3m2!1ses!2ses!4v1644359609428!5m2!1ses!2ses" width="200" height="200" style="border:0;" allowfullscreen="" loading="lazy"></iframe>        </div>
         <nav>
           <ul>
             <a href="instagram.com/?hl=es"><li><i class="fab fa-instagram fa-2x"></i></li></a>
@@ -12,4 +12,27 @@
           </ul>
         </nav>
         </div>
-      </footer>
+      </footer> --}}
+      <footer class="footer text-center">
+        <div class="container">
+            <div class="row">
+                <!-- Footer Location-->
+                
+                <!-- Footer Social Icons-->
+                <div class="col-lg-4 mb-5 mb-lg-0">
+                    <a  href="https://www.facebook.com/ekaitz.cormenzana"><i class="fab fa-fw fa-facebook-f fa-2x" id="facebook"></i></a>
+                    <a  href="https://twitter.com/Cormen1910"><i class="fab fa-fw fa-twitter fa-2x" id="twitter"></i></a>
+                    
+                    <a  href="https://www.instagram.com/cormen19/?hl=es"><i class="fab fa-fw fa-instagram fa-2x" id="instagram"></i></a>
+                </div>
+                <!-- Footer About Text-->
+                <div class="col-lg-4">
+                    <p id="correo" class="lead mb-0">
+                        GURE TABADUL
+                        
+                    </p>
+                </div>
+                
+            </div>
+        </div>
+    </footer>
