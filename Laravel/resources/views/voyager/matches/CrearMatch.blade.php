6a03dd4a4848eec7bfd9fad0b4e79ad6fc5f37a4
jordioni1180 <jordioni1705@gmail.com>
2022-02-08 08:06:42 +0100

    Update CrearMatch.blade.php

--- Laravel/resources/views/voyager/matches/CrearMatch.blade.php
+++ Laravel/resources/views/voyager/matches/CrearMatch.blade.php
@@ -4,19 +4,19 @@
     <div><input type="text" name="usuario1"></div>
     <button type="submit">Crear Match</button>
 
-    <form action=""></form>
     <select name="usuario1">
         @foreach ($usuarios as $user)
         <option value="{{$user->id}}">{{$user->email}}</option>
         @endforeach
     </select>
     </form>
-    <form>
+    <form route()>
     <select name="usuario2">
         @foreach ($usuarios as $user)
         <option value="{{$user->id}}">{{$user->email}}</option>
         @endforeach
     </select>
     </form>
-    <input type="button" value="buscar usuario" onClick="document.formulario.action='{{route}}'; document.formuario.submit()";>
+
+
 @endsection
