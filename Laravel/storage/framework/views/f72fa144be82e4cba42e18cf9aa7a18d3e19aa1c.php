730925546a8602806e7a4ee1a179d4e81f96a145
jordioni1180 <jordioni1705@gmail.com>
2022-02-09 21:30:20 +0100

    cosas

--- Laravel/storage/framework/views/f72fa144be82e4cba42e18cf9aa7a18d3e19aa1c.php
+++ Laravel/storage/framework/views/f72fa144be82e4cba42e18cf9aa7a18d3e19aa1c.php
@@ -5,11 +5,14 @@
 <?php $__env->stopSection(); ?>
 <?php $__env->startSection('contenido'); ?>
 <div class="taller_detalles">
+    <div class="fotoTaller">
+        
+    </div>
 <p id="titulo"><?php echo e($taller->Título); ?></p>
 <p id="fecha_inicio"><i class="fas fa-calendar-check"></i>&nbsp;&nbsp;<?php echo e($taller->Fecha_comienzo); ?></p>
 <p id="fecha_fin"><i class="fas fa-calendar-times"></i>&nbsp;&nbsp; <?php echo e($taller->Fecha_final); ?></p>
-<p id="descripcion"></p> 
-<p id="texto"><i class="fas fa-info-circle"></i>&nbsp; <?php echo e($taller->Descripción); ?></p>
+
+<p id="descripcion"><i class="fas fa-info-circle"></i>&nbsp; <?php echo e($taller->Descripción); ?></p>
      
 <p id="precio"><i class="fas fa-coins"></i>&nbsp; <?php echo e($taller->Precio); ?>&nbsp;&nbsp;<i class="fas fa-euro-sign"></i></p>
 <p id="capacidad"><i class="fas fa-female"></i>&nbsp;&nbsp;&nbsp; <?php echo e($taller->Capacidad); ?></p>
